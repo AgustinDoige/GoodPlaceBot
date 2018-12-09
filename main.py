f9ea1512eec3847dec0a239049ee9b346bee76d8
ADoige Desktop <laozoka@gmail.com>
2018-12-09 13:37:18 -0300

    Changed sidebarCountdown.py to be a module with function 'UpdateSidebar' to be used in main and added the sleepTime to main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import praw
+import datetime
+import pytz
+import json
+from time import sleep
+from sidebarCountdown import updateSidebar
 
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit('TheGoodSandbox') # Test sub: TheGoodSandbox
+print("Logged into r/{}".format(subreddit.title))
 
-pass
+sleepTime = 5
+while True:
+	updateSidebar(subreddit,sleepTime)
+	sleep(sleepTime)
