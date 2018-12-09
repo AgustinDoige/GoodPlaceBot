cea4359628ba614a38e8f2bd491ba5c6f70c62b0
ADoige Desktop <laozoka@gmail.com>
2018-12-09 17:36:08 -0300

    Added a basic rule for format comparison didnt change anything of note in pointsmodule.py

--- pointsmodule.py
+++ pointsmodule.py
@@ -5,6 +5,8 @@ import datetime
 
 def giveValue(string,ruleDic):
 	#dummyrule = {'name': "dummyRule",'shadow':False,'repply':None,'hardrule':True,'termlist':["shirt","fork","forking","shirtballs","ashole"],'reward':10}
+
+	for 
 	
 
 
@@ -16,6 +18,12 @@ def readSubreddit(subreddit,readtype):
 
 	#/r/subredditname/new and /r/subredditname/comments
 	pass
+
+
+
+with open("rules.json","r") as f:
+	rulesDic = json.load(f)
+
 
 
 
