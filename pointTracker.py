71861c32be3f46ebc84989a53838b21da92f31e9
ADoige Notebook <agu-amd@hotmail.com>
2018-12-09 16:32:41 -0300

    Fused softrules.txt and hardrules.txt as a json file caled rules.json and added comments to main.py

---
+++ pointTracker.py
@@ -0,0 +1,18 @@
+import praw
+import commentReader
+import json
+import datetime
+
+
+
+
+
+
+
+def readSubreddit(subreddit,readtype):
+
+	#/r/subredditname/new and /r/subredditname/comments
+	pass
+
+
+
---
+++ ruleMaker.py
@@ -0,0 +1,5 @@
+import json
+
+
+with open("rules.json","r") as f:
+	rulesDic = json.load(f)
