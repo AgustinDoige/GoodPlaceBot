b9b145fe861a48028e9d6390c7e7815cf1ab02ae
ADoige Notebook <agu-amd@hotmail.com>
2018-12-15 22:20:37 -0300

    Added the useful getSentences(str) function

--- pointsmodule.py
+++ pointsmodule.py
@@ -2,8 +2,31 @@ import praw
 import json
 import datetime
 
-def getSentences():
-	pass
+def getSentences(commentBody):
+	"""A function that takes a string and does its best to return a list of separate sentences. 
+		Used for some rules that need to look specifically inside a sentence"""
+	splitTerms = ['.','!','?','\n',':']
+	sentences = [commentBody]
+	rejectedList = ['www','com/','//']
+	for term in splitTerms:
+		for ind in range(0,len(sentences)):
+			tempStr = sentences.pop(0)
+			# sentences.extend(tempStr.split(term))
+			sencand = tempStr.split(term)
+			for it in sencand:
+				if (it != '' and it != ' '):
+					sentences.append(it)
+
+	curated = []
+	for sent in sentences:
+		add = True
+		for trm in rejectedList:
+			if trm in sent:
+				add = False
+		if add:
+			curated.append(sent)
+	return curated
+
 
 def giveValue(string,ruleDic):
 	#dummyrule = {'name': "dummyRule",'shadow':False,'repply':None,'hardrule':True,'termlist':["shirt","fork","forking","shirtballs","ashole"],'reward':10}
